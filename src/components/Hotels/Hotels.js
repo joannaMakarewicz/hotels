756fea021f019b14fca5d701bb7fbda7b53f6d2c
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-27 12:09:34 +0100

    Main structure of files

---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hotel from './Hotel/Hotel';
+
+const Hotels = () => {
+  return (
+    <div>
+        Hotels
+      <Hotel/>
+      <Hotel/>
+      <Hotel/>
+    </div>
+  )
+}
+
+export default Hotels;
