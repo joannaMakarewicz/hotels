11430fb3d878068755c0f86fe6f26dcb4388113b
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-27 15:32:15 +0100

    adding img & gh-pages

--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Hotel.css';
+import hotelImg from '../../../assets/img/hotel1.jpg';
 
 const Hotel = () => {
   return (
     <div className='hotel'>
+        <img className='img-fluid' src={hotelImg} alt=''/>
       Hotel
     </div>
   )
