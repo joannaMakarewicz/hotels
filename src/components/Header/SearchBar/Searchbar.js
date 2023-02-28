a39f5ec53460a7349a6bf8526f1b626a1f259884
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-28 13:17:43 +0100

    Searching basen od input value added

--- src/components/Header/SearchBar/Searchbar.js
+++ src/components/Header/SearchBar/Searchbar.js
@@ -1,14 +1,31 @@
-import './Searchbar.css';
+import React, { useState } from "react";
+import "./Searchbar.css";
 
 const SearchBar = () => {
+  const [input, setInput] = useState("");
+  const search = () => {
+    console.log("szukaj!", input);
+  };
+  const updateInput = (e) => {
+    setInput(e.target.value);
+  };
   return (
     <div className="searchBar d-flex">
       <input
-        className='searchBar__input'
+        value={input}
+        onChange={updateInput}
+        onKeyDown={e => e.key === "Enter" && search()}
+        className="searchBar__input"
         type="text"
         placeholder="Szukaj..."
       />
-      <button className='ms-1 btn btn-secondary searchBar__button' color='primary'>Szukaj</button>
+      <button
+        onClick={search}
+        className="ms-1 btn btn-secondary searchBar__button"
+        color="primary"
+      >
+        Szukaj
+      </button>
     </div>
   );
 };
