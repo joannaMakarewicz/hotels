54391f9b901a827ac71a8fc56b215b87ca7c538f
Joanna Makarewicz <joanna.milun@gmail.com>
2023-03-01 11:17:42 +0100

    added info for case of no hotels finded

--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -6,11 +6,12 @@ const Hotels = ({ hotels }) => {
   return (
     <div className="hotels container">
       <h2 className="hotels__heading">Oferty</h2>
-      {hotels.map((hotel) => {
-        return <Hotel hotel={hotel} key={hotel.id} />;
-      })}
+      {hotels.length !== 0 ? hotels.map((hotel) => {
+        return (
+        <Hotel hotel={hotel} key={hotel.id} />)}) : <p>Nie znaleziono noclegu.</p>
+        }
     </div>
   );
-};
+}
 
 export default Hotels;
