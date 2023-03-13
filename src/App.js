e5ceb2700177d98dffec0770d7c1550d60d86fb9
Joanna Makarewicz <joanna.milun@gmail.com>
2023-03-13 13:49:56 +0100

    structure of data in app.jschanged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header/Header";
 import SearchBar from "./components/Header/SearchBar/Searchbar";
@@ -8,60 +8,60 @@ import Menu from "./components/Menu/Menu";
 import Footer from "./components/Footer/Footer";
 import ColorItem from "./components/Header/ColorItem/ColorItem";
 
+const colors = [
+  {
+    id: "1",
+    color: "primary",
+  },
+  {
+    id: "2",
+    color: "danger",
+  },
+  {
+    id: "3",
+    color: "success",
+  },
+];
+
+const basicHotels = [
+  {
+    id: "1",
+    name: "Pod akacjami",
+    city: "Warszawa",
+    rating: "8.3.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "",
+  },
+  {
+    id: "2",
+    name: "Amazonka",
+    city: "Grudziądz",
+    rating: "8.7.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "",
+  },
+  {
+    id: "3",
+    name: "Pod palmami",
+    city: "Łeba",
+    rating: "9.3.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "",
+  },
+];
+
 function App() {
-
-
-  const colors = [
-    {
-      id: "1",
-      color: 'primary'
-    },
-    {
-      id: "2",
-      color: 'danger'
-    },
-    {
-      id: "3",
-      color: 'success'
-    }
-  ]
   const [theme, setTheme] = useState("primary");
+  const [hotels, setHotels] = useState(basicHotels);
+  // const [loading, setLoading] = useState(true);
+  // const [isAutenticated, setIsAutenticated] = useState(false);
 
- const changeTheme = (color) => {
+  const changeTheme = (color) => {
     setTheme(color);
   };
-
-  const basicHotels = [
-    {
-      id: "1",
-      name: "Pod akacjami",
-      city: "Warszawa",
-      rating: "8.3.",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "",
-    },
-    {
-      id: "2",
-      name: "Amazonka",
-      city: "Grudziądz",
-      rating: "8.7.",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "",
-    },
-    {
-      id: "3",
-      name: "Pod palmami",
-      city: "Łeba",
-      rating: "9.3.",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image: "",
-    },
-  ];
-
-  const [hotels, setHotels] = useState(basicHotels);
 
   const searchHandler = (input) => {
     const filteredHotels = basicHotels.filter((el) =>
@@ -71,18 +71,32 @@ function App() {
     setHotels(filteredHotels);
   };
 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setHotels(basicHotels);
+  //     setLoading(false);
+  //   }, 1000);
+  // }, []);
+
   return (
+    // <AuthContext.Provider value={{
+    //   isAutenticated : isAutenticated,
+    //   login: () => setIsAutenticated(true),
+    //   logout: () => setIsAutenticated(false),
+    // }}>
     <Layout
       header={
         <Header>
           <SearchBar onSearch={(input) => searchHandler(input)} theme={theme} />
-          <ColorItem colors={colors} changeTheme={changeTheme}/>
+          <ColorItem colors={colors} changeTheme={changeTheme} />
         </Header>
       }
       menu={<Menu theme={theme} />}
-      content={<Hotels hotels={hotels} theme={theme} />}
+      content={<Hotels hotels={hotels} theme={theme} />
+      }
       footer={<Footer theme={theme} />}
     />
+    // </AuthContext.Provider>
   );
 }
 
